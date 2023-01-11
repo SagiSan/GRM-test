d72f511ad299ea9912574e9f9e9238b9cd749e2c
Samir Sakanovic <samir.sagi@gmail.com>
2023-01-11 15:54:28 +0100

    grm test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("initial renders", () => {
+  test("renders header", () => {
+    render(<App />);
+    const header = screen.getByText(/GRM Test/i);
+    expect(header).toBeInTheDocument();
+  });
+  test("renders table", () => {
+    render(<App />);
+    const table = screen.getByTestId("table");
+    const tableBody = screen.getByTestId("table-body");
+    expect(table).toBeInTheDocument();
+    expect(tableBody.children.length).toBe(6);
+  });
+  test("renders start button", () => {
+    render(<App />);
+    const start = screen.getByText("Start");
+    expect(start).toBeInTheDocument();
+  });
+  test("form is hidden initially", () => {
+    render(<App />);
+    const form = screen.queryByTestId("form");
+    expect(form).not.toBeInTheDocument();
+  });
+});
+
+describe("compare functionality", () => {
+  test("form visible on start click", () => {
+    render(<App />);
+    userEvent.click(screen.getByText("Start"));
+    const form = screen.queryByTestId("form");
+    const formSubmit = screen.getByTestId("form-submit");
+    expect(form).toBeInTheDocument();
+    expect(formSubmit).toBeDisabled();
+  });
+  test("submit values", () => {
+    render(<App />);
+    userEvent.click(screen.getByText("Start"));
+    const tableBody = screen.getByTestId("table-body");
+    const input1 = screen.getByTestId("input1");
+    const input2 = screen.getByTestId("input2");
+    const formSubmit = screen.getByTestId("form-submit");
+    userEvent.type(input1, "2");
+    userEvent.type(input2, "1");
+    expect(input1).toHaveValue(2);
+    expect(input2).toHaveValue(1);
+    userEvent.click(formSubmit);
+    const scoreCell = tableBody.children[0].children[2];
+    expect(scoreCell).toHaveTextContent("1");
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FormEvent, useState } from "react";
+import "./App.css";
+
+interface Item {
+  name: string;
+  score: number;
+}
 
 function App() {
+  const [items, setItems] = useState<Item[]>([
+    { name: "Item1", score: 0 },
+    { name: "Item2", score: 0 },
+    { name: "Item3", score: 0 },
+    { name: "Item4", score: 0 },
+    { name: "Item5", score: 0 },
+    { name: "Item6", score: 0 },
+  ]);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [compareIndex1, setCompareIndex1] = useState(0);
+  const [compareIndex2, setCompareIndex2] = useState(1);
+
+  const onStart = () => {
+    setIsFormVisible(true);
+  };
+
+  const onFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const first = parseFloat(input1);
+    const second = parseFloat(input2);
+
+    if (first !== second) {
+      const higher = first > second ? compareIndex1 : compareIndex2;
+      const data = [...items];
+      data[higher].score++;
+      setItems(data);
+    }
+    setInput1("");
+    setInput2("");
+
+    if (compareIndex2 !== 5) {
+      setCompareIndex2(compareIndex2 + 1);
+    } else if (compareIndex1 !== 4) {
+      setCompareIndex1(compareIndex1 + 1);
+      setCompareIndex2(compareIndex1 + 2);
+    } else {
+      //end
+      setIsFormVisible(false);
+    }
+  };
+
+  const sortedItems = [...items].sort((a, b) => (a.score > b.score ? -1 : 1));
+  const isEnd = compareIndex1 === 4 && compareIndex2 === 5 && !isFormVisible;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GRM Test</h1>
+      <table data-testid="table">
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody data-testid="table-body">
+          {sortedItems.map((item, index) => (
+            <tr key={item.name}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {!isEnd && !isFormVisible ? (
+        <button className="start-btn" onClick={onStart}>
+          Start
+        </button>
+      ) : null}
+      {isFormVisible ? (
+        <form onSubmit={onFormSubmit} data-testid="form">
+          <label>{items[compareIndex1].name} </label>
+          <input
+            data-testid="input1"
+            className="inputs"
+            type="number"
+            value={input1}
+            onChange={(e) => setInput1(e.currentTarget.value)}
+          />
+          <input
+            data-testid="input2"
+            className="inputs"
+            type="number"
+            value={input2}
+            onChange={(e) => setInput2(e.currentTarget.value)}
+          />
+          <label> {items[compareIndex2].name}</label>
+          <br />
+          <input
+            data-testid="form-submit"
+            className="form-submit"
+            type="submit"
+            disabled={input1 === "" || input2 === ""}
+          />
+        </form>
+      ) : null}
     </div>
   );
 }
